718dbb8d07c5d5528eb0d988025439026ad16e0a
MarceloFreitas <marcelohfreitas235@gmail.com>
2025-05-09 17:55:50 -0300

    First Commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Meu Sistema</h1>
+@stop
+
+@section('content')
+   <p>Bem vindo ao sistema de gerenciamento de estoque e vendas!</p>
+@stop
